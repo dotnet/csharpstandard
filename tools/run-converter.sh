0bfdf30323455bf74b7bd75c932eca542475cf3a
Jon Skeet <skeet@pobox.com>
2020-11-17 13:34:46 +0000

    Fixes to the MarkdownConverter and convenience script
    I'm not sure how the converter has *ever* run without the change
    here - I'd expect it to try to load the template as Markdown.

--- tools/run-converter.sh
+++ tools/run-converter.sh
@@ -5,12 +5,12 @@ set -e
 # This is a convenient script to test the conversion from Markdown to Word.
 # The script assumes that:
 # - It's being run from the directory in which it's present
-# - The spec Markdown is in ../conversion-to-markdown
+# - The spec Markdown is in ../standard
 # - It can wipe and recreate a tmp directory
 # - You want to use the default template (currently in the MarkdownConverter directory)
 
 declare -r CONVERTER_PROJECT=MarkdownConverter
-declare -r SPEC_DIRECTORY=../conversion-to-markdown
+declare -r SPEC_DIRECTORY=../standard
 declare -r TEMPLATE=$CONVERTER_PROJECT/template.docx
 declare -r OUTPUT_DIRECTORY=tmp
 
@@ -19,5 +19,5 @@ mkdir $OUTPUT_DIRECTORY
 
 dotnet run -p $CONVERTER_PROJECT -- \
   $SPEC_DIRECTORY/*.md $TEMPLATE \
-  -o $OUTPUT_DIRECTORY/spec.docx
+  -o $OUTPUT_DIRECTORY/standard.docx
 
