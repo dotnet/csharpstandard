c5064bba38c94c2c57b43878b3577d639ced9a92
Bill Wagner <wiwagn@microsoft.com>
2021-06-16 11:58:44 -0400

    first code ready
    Log any dry run errors where section references can't be found.

--- tools/StandardAnchorTags/ReferenceUpdateProcessor.cs
+++ tools/StandardAnchorTags/ReferenceUpdateProcessor.cs
@@ -13,6 +13,7 @@ namespace StandardAnchorTags
         private readonly IReadOnlyDictionary<string, SectionLink> linkMap;
         private readonly bool dryRun;
         private readonly string PathToFiles;
+        public int ErrorCount { get; private set; }
 
         public ReferenceUpdateProcessor(string pathToFiles, IReadOnlyDictionary<string, SectionLink> linkMap, bool dryRun)
         {
@@ -59,22 +60,30 @@ namespace StandardAnchorTags
             {
                 // Grab the section text:
                 string referenceText = line[sectionReferenceRange];
+                string linkText = referenceText;
                 if ((referenceText.Length > 1) &&
                     (!linkMap.ContainsKey(referenceText)))
                 {
-                    throw new InvalidOperationException($"Section reference [{referenceText}] not found at line {lineNumber} in {file}");
+                    var msg = $"Section reference [{referenceText}] not found at line {lineNumber} in {file}";
+                    if (dryRun)
+                    {
+                        ErrorCount++;
+                        Console.WriteLine(msg);
+                    }
+                    else
+                        throw new InvalidOperationException(msg);
+                } else
+                {
+                    linkText = linkMap[referenceText].FormattedMarkdownLink;
                 }
-                // Optionally expand the range for an existing link:
+                // expand the range for any existing link:
                 sectionReferenceRange = ExpandToIncludeExistingLink(line, sectionReferenceRange);
 
                 var textRangeToCopyUnchanged = new Range(index, sectionReferenceRange.Start);
                 // Copy text up to replacement:
                 returnedLine.Append(line[textRangeToCopyUnchanged]);
 
-                // Oddly, we have a few blank references in the standard:
-                returnedLine.Append(referenceText.Length > 1 
-                    ? linkMap[referenceText].FormattedMarkdownLink
-                    : referenceText);
+                returnedLine.Append(linkText);
                 index = sectionReferenceRange.End.Value;
             }
             // Copy remaining text
@@ -90,16 +99,21 @@ namespace StandardAnchorTags
             if (startIndex == -1) return default;
             int endIndex = startIndex + 1;
 
-            // Find the end:
-            // Special case the first character, because it could be an Annex:
-            if (line[endIndex] >= 'A' && line[endIndex] <= 'Z') endIndex++;
-
-            // Remaining must be 0..9, or .: (TODO: update with range pattern in C# 9.0 in time)
-            while ((endIndex < line.Length) &&
-                ((line[endIndex] >= '0' && line[endIndex] <= '9') || (line[endIndex] == '.')))
+            // The first character not in the set of:
+            // A..Z
+            // a..z
+            // 0..9
+            // .
+            // - 
+            // indicates the end of the link reference.
+            while (line[endIndex] switch
             {
-                endIndex++;
-            }
+                >= 'A' and <= 'Z' => true,
+                >= 'a' and <= 'z' => true,
+                >= '0' and <= '9' => true,
+                '.' or '-' => true,
+                _ => false,
+            }) endIndex++;
 
             // One final special case: If the last character is '.', it's not 
             // part of the section reference, it's the period at the end of a sentence:
