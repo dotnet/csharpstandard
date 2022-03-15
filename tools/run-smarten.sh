247db2da9ce3aa90494d5b0a5d2c987949db7794
Bill Wagner <wiwagn@microsoft.com>
2022-03-15 12:59:40 -0400

    finish local script testing

--- tools/run-smarten.sh
+++ tools/run-smarten.sh
@@ -2,46 +2,54 @@
 set -eu -o pipefail
 
 declare -r SPEC_DIRECTORY=../standard
+declare -r EXTENSION=md
 
 declare -a SPEC_FILES=(
-    "foreword.md"
-    "introduction.md"
-    "scope.md"
-    "normative-references.md"
-    "terms-and-definitions.md"
-    "general-description.md"
-    "conformance.md"
-    "lexical-structure.md"
-    "basic-concepts.md" 
-    "types.md"
-    "variables.md"
-    "conversions.md"
-    "expressions.md"
-    "statements.md"
-    "namespaces.md"
-    "classes.md"
-    "structs.md"
-    "arrays.md"
-    "interfaces.md"
-    "enums.md"
-    "delegates.md"
-    "exceptions.md"
-    "attributes.md"
-    "unsafe-code.md"
-    "grammar.md"
-    "portability-issues.md"
-    "standard-library.md"
-    "documentation-comments.md"
-    "bibliography.md"
+    "foreword"
+    "introduction"
+    "scope"
+    "normative-references"
+    "terms-and-definitions"
+    "general-description"
+    "conformance"
+    "lexical-structure"
+    "basic-concepts" 
+    "types"
+    "variables"
+    "conversions"
+    "expressions"
+    "statements"
+    "namespaces"
+    "classes"
+    "structs"
+    "arrays"
+    "interfaces"
+    "enums"
+    "delegates"
+    "exceptions"
+    "attributes"
+    "unsafe-code"
+    "grammar"
+    "portability-issues"
+    "standard-library"
+    "documentation-comments"
+    "bibliography"
     )
 
 # unpack the package to ./smarten
 tar -xvf ../.github/workflows/dependencies/EcmaTC49.Smarten.tar
 
+# On some files, smarten can't "round trip", so creates
+# a new file of the form <filename>_(RoundTrip).md.
+# The extra logic is to move those over the originals.
 for file in "${SPEC_FILES[@]}"
 do
-   echo "$file"
-   ./smarten/Smarten_CL.exe $SPEC_DIRECTORY/$file $SPEC_DIRECTORY/$file 
+    echo "$file"
+    ./smarten/Smarten_CL.exe $SPEC_DIRECTORY/$file.$EXTENSION $SPEC_DIRECTORY/$file.$EXTENSION
+    # If $file_(RoundTrip).md was created, mv:
+    if [ -f $SPEC_DIRECTORY/$file"_(RoundTrip)."$EXTENSION ]; then
+       mv $SPEC_DIRECTORY/$file"_(RoundTrip)."$EXTENSION $SPEC_DIRECTORY/$file.$EXTENSION
+    fi
 done
 
 
