9a7da52c233462dcf0de89bb1d0680a4ef5de194
Rex Jaeschke <rex@RexJaeschke.com>
2022-10-22 16:34:30 -0400

    Correctly handle "  >" inside an unnumbered list item

--- tools/ExampleExtractor/Example.cs
+++ tools/ExampleExtractor/Example.cs
@@ -91,6 +91,8 @@ internal class Example
             string TrimPrefix(string codeLine) =>
                 codeLine.StartsWith(prefix) ? codeLine.Substring(prefix.Length)
                         : codeLine.StartsWith(trimmedPrefix) ? codeLine.Substring(trimmedPrefix.Length)
+            // the following line handles the case of an example's blank line ("  >") inside an unnumbered list item (which is indented by 2 spaces)
+                        : codeLine.StartsWith("  " + trimmedPrefix) ? codeLine.Substring(2 + trimmedPrefix.Length)
                         : throw new InvalidOperationException($"Example in {markdownFile} starting at line {openingLine} contains line without common prefix");
 
         }
