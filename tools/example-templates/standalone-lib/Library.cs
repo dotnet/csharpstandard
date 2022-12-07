4500d39d901ccb41a71a929f73fdf7af339604ad
Rex Jaeschke <rex@RexJaeschke.com>
2022-12-07 08:38:04 -0500

    add commonly used using directives

--- tools/example-templates/standalone-lib/Library.cs
+++ tools/example-templates/standalone-lib/Library.cs
@@ -1,1 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading;
+
 $example-code
