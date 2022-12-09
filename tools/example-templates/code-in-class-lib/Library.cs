71ac8874229ab2a44163c23a4fda8ad6b9ef190c
Rex Jaeschke <rex@RexJaeschke.com>
2022-12-09 10:51:37 -0500

    add core using directives
    this mirrors the changes in PR #[643](https://github.com/dotnet/csharpstandard/pull/643).

--- tools/example-templates/code-in-class-lib/Library.cs
+++ tools/example-templates/code-in-class-lib/Library.cs
@@ -1,3 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading;
+
 class Class1
 {
     $example-code
