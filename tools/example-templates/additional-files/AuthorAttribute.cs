a61efd08a871926b05e767575ebcc07f0214e364
Rex Jaeschke <rex@RexJaeschke.com>
2023-01-07 10:47:20 -0500

    Update AuthorAttribute.cs

--- tools/example-templates/additional-files/AuthorAttribute.cs
+++ tools/example-templates/additional-files/AuthorAttribute.cs
@@ -2,13 +2,6 @@ using System;
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
 public class AuthorAttribute : Attribute
 {
-    private string name;
-    public AuthorAttribute(string name)
-    {
-        this.name = name;
-    }
-    public string Name
-    {
-        get { return name; }
-    }
+     public string Name { get; }
+     public AuthorAttribute(string name) => Name = name;
 }
