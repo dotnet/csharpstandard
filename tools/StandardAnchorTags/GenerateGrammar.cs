8deef3abb41c0e6c8b9e1de741d0dcbb99c1e140
Bill Wagner <wiwagn@microsoft.com>
2023-10-27 17:03:48 -0400

    Replicate current functionality
    The section renumbering tool now replicates the output from the grammar extraction tool.
    
    Refactoring to come.

---
+++ tools/StandardAnchorTags/GenerateGrammar.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StandardAnchorTags
+{
+    public class GenerateGrammar : IDisposable
+    {
+        private readonly string pathToStandardFiles;
+        private readonly StreamWriter grammarStream;
+        private const string LexicalHeader = 
+@"# Annex A Grammar
+
+**This clause is informative.**
+
+## A.1 General
+
+This annex contains the grammar productions found in the specification, including the optional ones for unsafe code. Productions appear here in the same order in which they appear in the specification.
+
+## A.2 Lexical grammar
+
+```ANTLR";
+
+        private const string SyntacticHeader =
+@"```
+
+## A.3 Syntactic grammar
+
+```ANTLR";
+
+        private const string UnsafeExtensionsHeader =
+@"```
+
+## A.4 Grammar extensions for unsafe code
+
+```ANTLR";
+
+        private const string GrammarFooter =
+@"```
+
+**End of informative text.**
+";
+
+        public GenerateGrammar(string grammarPath, string pathToStandardFiles)
+        {
+            grammarStream = new StreamWriter(grammarPath, false);
+            this.pathToStandardFiles = pathToStandardFiles;
+        }
+
+        public async Task WriteHeader() => await grammarStream.WriteLineAsync(GenerateGrammar.LexicalHeader);
+        public async Task WriteSyntaxHeader() => await grammarStream.WriteLineAsync(GenerateGrammar.SyntacticHeader);
+        public async Task WriteUnsafeExtensionHeader() => await grammarStream.WriteLineAsync(GenerateGrammar.UnsafeExtensionsHeader);
+
+        public async Task WriteGrammarFooter()
+        {
+            await grammarStream.WriteLineAsync(GenerateGrammar.GrammarFooter);
+            await grammarStream.FlushAsync();
+            grammarStream.Close();
+        }
+
+        public async Task ExtractGrammarFrom(string inputFileName)
+        {
+            string inputFilePath = $"{pathToStandardFiles}/{inputFileName}";
+            using var inputFile = new StreamReader(inputFilePath);
+            string section = "";
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+            while (await inputFile.ReadLineAsync() is string inputLine)
+            {
+                if (inputLine.StartsWith("#"))
+                {
+                    section = inputLine.Trim('#', ' ');
+                    continue;
+                }
+                if (!inputLine.StartsWith("```ANTLR", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    continue;
+                }
+                //		Console.WriteLine("------ Start of a production");
+                await grammarStream.WriteLineAsync();    // write out blank line before each new production
+                await grammarStream.WriteLineAsync($"// Source: §{section}");
+
+                // This loop might be a candidate for a bit of refactoring.
+                while (true)
+                {
+                    string? nextLine = await inputFile.ReadLineAsync();
+                    if (nextLine == null)
+                    {
+                        throw new InvalidOperationException("Unexpected EOF; no closing grammar fence");
+                    }
+                    if (nextLine.Length < 3)   // Is it long enough to contain a closing fence?
+                    {
+                        await grammarStream.WriteLineAsync(nextLine);
+                    }
+                    else if (nextLine.Substring(0, 3) == "```")    // If line starts with ```
+                    {
+                        //			Console.WriteLine("------ End of a production");
+                        break;
+                    }
+                    else
+                    {
+                        await grammarStream.WriteLineAsync(nextLine);
+                    }
+                }
+            }
+        }
+
+        public void Dispose() => grammarStream.Dispose();
+    }
+}
--- tools/Utilities/Clauses.cs
+++ tools/Utilities/Clauses.cs
@@ -3,6 +3,10 @@
     public class Clauses
     {
         public string[] FrontMatter { get; set; } = Array.Empty<string>();
+
+        public string[] ScopeAndConformance {  get; set; } = Array.Empty<string>();
+
+        public string[] LexicalStructure { get; set; } = Array.Empty<string>();
         public string[] MainBody { get; set; } = Array.Empty<string>();
 
         // Sure, there's only one, but let's allow for possible expansion.
