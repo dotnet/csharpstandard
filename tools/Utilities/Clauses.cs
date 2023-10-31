2fb8425d1436a6ab5a69e48449463c1e1b7ac92b
Bill Wagner <wiwagn@microsoft.com>
2023-10-31 11:05:43 -0400

    Combine merge tools (#977)
    * Update clauses to separate unsafe code
    
    For the grammar, we separate the unsafe code extensions. Make that distinction in the clauses.json, the code that reads it, and the section renumbering tool.
    
    * Replicate current functionality
    
    The section renumbering tool now replicates the output from the grammar extraction tool.
    
    Refactoring to come.
    
    * remove update grammar tool
    
    It's no longer needed because it's functionality has been replicated in the section renumber tool.
    
    * Refactoring, part 1
    
    Simplify the loop that reads all the grammar productions from parts of the standard, and writes them to the grammar annex.
    
    * refactoring, part 2
    
    Read the existing text outside of the ANTLR productions from the existing annex.
    
    That way, we can make any edits  to this text (including any changing section numbers) in the markdown file, rather than in the program source code.
    
    * some final refactoring
    
    * respond to feedback
    
    * one final small lint fix

--- tools/Utilities/Clauses.cs
+++ tools/Utilities/Clauses.cs
@@ -3,7 +3,14 @@
     public class Clauses
     {
         public string[] FrontMatter { get; set; } = Array.Empty<string>();
+
+        public string[] ScopeAndConformance {  get; set; } = Array.Empty<string>();
+
+        public string[] LexicalStructure { get; set; } = Array.Empty<string>();
         public string[] MainBody { get; set; } = Array.Empty<string>();
+
+        // Sure, there's only one, but let's allow for possible expansion.
+        public string[] UnsafeClauses { get; set; } = Array.Empty<string>();
         public string[] Annexes { get; set; } = Array.Empty<string>();
     }
 }
