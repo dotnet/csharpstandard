4045e23034e8682fff39c9d08198ed35d43ad8bb
Bill Wagner <wiwagn@microsoft.com>
2024-06-06 10:13:55 -0400

    Update YML and shell script

--- tools/run-section-renumber.sh
+++ tools/run-section-renumber.sh
@@ -4,12 +4,7 @@ set -e
 
 declare -r PROJECT=StandardAnchorTags
 
-if [ "$1" == "--dryrun" ]
-then
-    echo "Performing a dry run"
-fi
-
-dotnet run --project $PROJECT -- $1
+dotnet run --project $PROJECT -- --owner dotnet --repo csharpstandard --dryrun true --head-sha $1
 
 if [ -n "$GITHUB_OUTPUT" ]
 then
