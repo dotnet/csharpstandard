2e0f7aa09da4a0ad544af7413813395fb013c92f
Bill Wagner <wiwagn@microsoft.com>
2024-06-24 16:40:17 -0400

    Fix hack
    Fix the hack in #1134 with more reasonable shell programming.

--- tools/run-section-renumber.sh
+++ tools/run-section-renumber.sh
@@ -3,8 +3,14 @@
 set -e
 
 declare -r PROJECT=StandardAnchorTags
+declare -r STATUS_CHECK_PARMS=""
 
-dotnet run --project $PROJECT -- --owner dotnet --repo csharpstandard --dryrun true --head-sha $1
+if [ -z "$1" ]
+then
+    STATUS_CHECK_PARMS="--dry-run --head-sha $1"
+fi
+
+dotnet run --project $PROJECT -- --owner dotnet --repo csharpstandard $STATUS_CHECK_PARMS
 
 if [ -n "$GITHUB_OUTPUT" ]
 then
