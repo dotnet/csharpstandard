835850ee1d266737e8b543cac180a97fb1ed92fa
Nigel-Ecma <Nigel-Ecma@users.noreply.github.com>
2025-03-08 11:00:43 +1300

    [BG 2.3.0] • Handle `is` properly for v8, will change in v9 and hopefully not require fixup • Add support for disambiguation of type_argument_list with a query_expression • Add (nonsensical) sample to test the above

--- tools/GrammarTesting/Tests/Parsing/Samples/v8/Type argument list/ReadMe.md
+++ tools/GrammarTesting/Tests/Parsing/Samples/v8/Type argument list/ReadMe.md
@@ -2,3 +2,5 @@
 
 Uses modification set Rules which implements the disambiguation algorithm
 to determine whether a sequence of tokens is a *type_argument_list* or some expression.
+
+Many of the examples are taken from the Standard’s exmaples or from the source material.
--- tools/GrammarTesting/Tests/Parsing/Samples/v8/Type argument list/sample.cs
+++ tools/GrammarTesting/Tests/Parsing/Samples/v8/Type argument list/sample.cs
@@ -1,36 +1,36 @@
 class TypeArgumentListChecks
 {
    void TypeArgumentListChecks()
-   {     
+   {
       F(G<A, B>(7));       // a call to F with one argument, which is a call to a generic
                            // method G with two type arguments and one regular argument
       F(base.G<A, B>(7));  // ditto
-      
+
       F(G<A, B>7);         // a call to F with two arguments
       F(base.G<A, B>7);    // ditto
-      F(G<A, B>>7);        // ditto 
-      
+      F(G<A, B>>7);        // ditto
+
       x = F<A> + y;        // a less-than operator, greater-than operator and unary-plus
                            // operator, as if the statement had been written:
                            //    x = (F < A) > (+y)
-      
+
       x = y is C<T> && z;           // a namespace_or_type_name with a type_argument_list
-      
-      var v = (A < B, C); 
-            
+
+      var v = (A < B, C);
+
       var v = (A < B, C > D);       // a tuple with two elements, each a comparison.
-     
+
       var v = (A<B,C> D, E);        // tuple with two elements, the first of which is
                                     // a declaration expression.
 
       var v = M(A < B, C > D, E);   // call with three arguments.
-      
+
       var v = M(out A<B,C> D, E);   // call with two arguments, the first of which is
                                     // an out declaration.
-      
+
       if (e is A<B> C)              // a declaration pattern.
          W(C);
-      
+
       if (e is A<B>)                // a type
          W(C);
 
@@ -41,5 +41,9 @@ class TypeArgumentListChecks
             break;
       }
 
+      var p = x is A<B>>C;          // (x is A<B>) > C
+      var q = A<B>>C;               // A < (B >> C)
+      var r = (x is A<B>)>C;        // (x is A<B>) > C
+      var s = x is A<B> orderby;    // declaration pattern defining `orderby`
    }
 }
