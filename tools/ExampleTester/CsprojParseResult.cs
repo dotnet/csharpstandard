aed3a7fd1b198f1f957e820a1c0aed4897d55f5d
jnm2 <me@jnm2.com>
2025-06-01 15:00:10 -0400

    25x nonparallelized speedup by avoiding MSBuildWorkspace except for multi-project examples

---
+++ tools/ExampleTester/CsprojParseResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace ExampleTester;
+
+internal sealed record CsprojParseResult(
+    string? AssemblyName,
+    string TargetFramework,
+    CSharpParseOptions ParseOptions,
+    CSharpCompilationOptions CompilationOptions,
+    ImmutableArray<SyntaxTree> GeneratedSources);
---
+++ tools/ExampleTester/FastCsprojCompilationParser.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Frozen;
+using System.Collections.Immutable;
+using System.Globalization;
+using System.Xml.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
+
+namespace ExampleTester;
+
+internal static class FastCsprojCompilationParser
+{
+    public static CSharpCompilation CreateCompilation(string csprojPath)
+    {
+        var csproj = ParseCsproj(csprojPath);
+
+        var syntaxTrees = Directory.GetFiles(Path.GetDirectoryName(csprojPath)!, "*.cs").AsParallel().Select(path =>
+        {
+            using var stream = File.OpenRead(path);
+            return CSharpSyntaxTree.ParseText(SourceText.From(stream), csproj.ParseOptions, path);
+        });
+
+        return CSharpCompilation.Create(
+            csproj.AssemblyName,
+            csproj.GeneratedSources.AddRange(syntaxTrees),
+            SupportedTargetFrameworks[csproj.TargetFramework].References,
+            csproj.CompilationOptions);
+    }
+
+    private static readonly FrozenDictionary<string, OutputKind> SupportedOutputKinds = new KeyValuePair<string, OutputKind>[]
+    {
+        new("Exe", OutputKind.ConsoleApplication),
+        new("Library", OutputKind.DynamicallyLinkedLibrary),
+        new("WinExe", OutputKind.WindowsApplication),
+    }.ToFrozenDictionary(StringComparer.OrdinalIgnoreCase);
+
+    private sealed record TargetFrameworkInfo(
+        ImmutableArray<PortableExecutableReference> References,
+        LanguageVersion DefaultLanguageVersion,
+        int DefaultWarningLevel);
+
+    private static readonly FrozenDictionary<string, TargetFrameworkInfo> SupportedTargetFrameworks = new KeyValuePair<string, TargetFrameworkInfo>[]
+    {
+        new("net6.0", new(Basic.Reference.Assemblies.Net60.References.All, LanguageVersion.CSharp10, DefaultWarningLevel: 6)),
+    }.ToFrozenDictionary(StringComparer.OrdinalIgnoreCase);
+
+    private static CsprojParseResult ParseCsproj(string filePath)
+    {
+        var csprojDocument = XDocument.Load(filePath);
+
+        string? assemblyName = null;
+        string? targetFramework = null;
+        var parseOptions = CSharpParseOptions.Default;
+        var compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, deterministic: true);
+        var implicitUsings = false;
+
+        foreach (var element in csprojDocument.Root!.Elements())
+        {
+            switch (element.Name.LocalName)
+            {
+                case "PropertyGroup":
+                    foreach (var property in element.Elements())
+                    {
+                        switch (property.Name.LocalName)
+                        {
+                            case "OutputType":
+                                compilationOptions = compilationOptions.WithOutputKind(SupportedOutputKinds[property.Value]);
+                                break;
+                            case "TargetFramework":
+                                targetFramework = property.Value;
+                                break;
+                            case "Nullable":
+                                compilationOptions = compilationOptions.WithNullableContextOptions(
+                                    Enum.Parse<NullableContextOptions>(property.Value, ignoreCase: true));
+                                break;
+                            case "AssemblyName":
+                                assemblyName = property.Value;
+                                break;
+                            case "AllowUnsafeBlocks":
+                                compilationOptions = compilationOptions.WithAllowUnsafe(bool.Parse(property.Value));
+                                break;
+                            case "LangVersion":
+                                var enumMemberName = property.Value;
+
+                                if (decimal.TryParse(
+                                    property.Value,
+                                    NumberStyles.AllowLeadingWhite | NumberStyles.AllowTrailingWhite | NumberStyles.AllowDecimalPoint,
+                                    CultureInfo.InvariantCulture,
+                                    out var decimalValue))
+                                {
+                                    enumMemberName = "CSharp" + decimalValue.ToString("0.#", CultureInfo.InvariantCulture).Replace('.', '_');
+                                }
+
+                                parseOptions = parseOptions.WithLanguageVersion(Enum.Parse<LanguageVersion>(enumMemberName, ignoreCase: true));
+                                break;
+                            case "ImplicitUsings":
+                                implicitUsings = ParseFeatureBool(property.Value);
+                                break;
+                            default:
+                                throw new NotImplementedException($"Review whether support is required for <{property.Name}> ({filePath})");
+                        }
+                    }
+                    break;
+
+                default:
+                    throw new NotImplementedException($"Review whether support is required for <{element.Name}> ({filePath})");
+            }
+        }
+
+        if (parseOptions.SpecifiedLanguageVersion == LanguageVersion.Default)
+            parseOptions = parseOptions.WithLanguageVersion(SupportedTargetFrameworks[targetFramework!].DefaultLanguageVersion);
+
+        compilationOptions = compilationOptions.WithWarningLevel(SupportedTargetFrameworks[targetFramework!].DefaultWarningLevel);
+
+        var generatedSources = ImmutableArray.CreateBuilder<SyntaxTree>();
+
+        if (implicitUsings)
+        {
+            generatedSources.Add(SyntaxFactory.ParseSyntaxTree("""
+                global using System;
+                global using System.Collections.Generic;
+                global using System.IO;
+                global using System.Linq;
+                global using System.Net.Http;
+                global using System.Threading;
+                global using System.Threading.Tasks;
+                """,
+                parseOptions,
+                assemblyName + ".GlobalUsings.g.cs"));
+        }
+
+        return new CsprojParseResult(
+            assemblyName,
+            targetFramework!,
+            parseOptions,
+            compilationOptions,
+            generatedSources.DrainToImmutable());
+    }
+
+    private static bool ParseFeatureBool(string propertyValue)
+    {
+        return propertyValue.Equals("enable", StringComparison.OrdinalIgnoreCase)
+            || propertyValue.Equals("true", StringComparison.OrdinalIgnoreCase);
+    }
+}
