bd5df75871b901a90c2bf09a675f1d2541e04003
Nigel-Ecma <Nigel-Ecma@users.noreply.github.com>
2026-01-29 15:42:15 +1300

    Rebased #1538

--- tools/GrammarTesting/Tests/Parsing/Samples/v8/Declaration expressions/sample.cs
+++ tools/GrammarTesting/Tests/Parsing/Samples/v8/Declaration expressions/sample.cs
@@ -14,9 +14,25 @@ class DeclarationExpressions
    void DeclarationExpressions_2()
    {
       R(A < B, C > D, E) = x;                      // method call, 3 arguments
-      var y = (A < B, C > D, E);                   // 3-tuple – no decl expr allowed on RHS
-      (A < B, C > D, E) = y;                       // similar tuple is 2-tuple on LHS – declaration expression & E (C#10)
+      (A < B, C > D, _) = y;                       // similar tuple is 2-tuple on LHS – declaration expression & _
 
-      (A < B, C > D, E, F < G, H > I) = y;         // 3-tuple – decl expr, E, decl expr (C#10)
+      (A < B, C > D, _, F < G, H > I) = y;         // 3-tuple – decl expr, _, decl expr
+
+      t = (1, 2, (3, 4));
+      (p, _, (_, q)) = t;                          // deconstructing assignment
+      (var p, _, (_, var q)) = t;                  // deconstructing declaration
+      (var p, _, _) = (_, _, (_, q)) = t;          // both
+
+      (A < B, C > p, _, (_, A < B, C > q)) = t;                // deconstructing declaration w/TAL
+      (A < B, C > p, _, _) = (_, _, (_, q)) = t;               // valid
+
+      (A < B, C > p, _, _) = (_, _, (_, A < B, C > q)) = t;    // syntax valid, semantically invalid:
+                                                               // A < B & C > q are not a variable references
+
+      (var p, _, _) = (_, A < B, C > q) = x;                   // syntax valid, semantically invalid:
+                                                               // A < B & C > q are not variable references
+
+      (var p, _, _) = (_, a < b ? ref q : ref r, _) = x;       // valid, `a < b ? ref q : ref r` is ref-valued
+
    }
 }
